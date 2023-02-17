b566f1692c94c75b734444a668981811b9306ca2
Maksim Borodin <sqept1k@gmail.com>
2023-02-17 21:26:31 +0300

    exercise*

---
+++ script.sh
@@ -0,0 +1,16 @@
+mkdir dir0
+echo 'dir0 was created'
+cd ./dir0
+echo 'moved to dir0'
+mkdir dir1 dir2 dir3
+echo 'dir1 dir2 dir3 was created'
+cd dir1
+echo 'moved to dir1'
+touch file.txt file1.txt file2.txt git.json bash.json
+echo 'file.txt file1.txt file2.txt git.json bash.json waree created'
+mkdir dir4 dir5 dir6
+echo 'dir4 dir5 dir6 was created'
+ls
+echo 'extract the contents of the folder'
+mv file.txt git.json dir4
+echo 'perenesli 2 file v drugyu papky'
